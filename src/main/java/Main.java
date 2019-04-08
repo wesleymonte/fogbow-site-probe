1f9bb57aa3a0ea024d36ccce7a0474a7a39b3b6e
Raoni Smaneoto <raoni.smaneoto@ccc.ufcg.edu.br>
2019-04-08 16:49:38 -0300

    Adding some missing dependecies

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+package .;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Properties;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import br.edu.ufcg.lsd.core.ProbeController;
+
+public class Main {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+
+    }
+}
