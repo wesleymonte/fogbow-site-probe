f0c77811d8faa9143b0bcca6984e32a7daa48a35
raonismaneoto <raoni.smaneoto@ccc.ufcg.edu.br>
2019-05-28 15:43:48 -0300

    general fixes

--- install.sh
+++ install.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
+USER=ubuntu
 
-ssh_key_file=ip=awk -F ' *= *' '$1=="ssh-private-key"{print $2}' ./probe-fogbow.conf
-ip=awk -F ' *= *' '$1=="host-ip"{print $2}' ./probe-fogbow.conf
-ssh -i ssh_key_file $USER@ip
+ssh_key_file=`awk -F ' *= *' '$1=="ssh-private-key"{print $2}' ./probe-fogbow.conf`
+ip=`awk -F ' *= *' '$1=="host-ip"{print $2}' ./probe-fogbow.conf`
 
-fogbow-probe-repo=https://github.com/eubr-atmosphere/fogbow-site-probe.git
-(cd ~/ && git clone $fogbow-probe-repo && exit 0)
+ssh -i $ssh_key_file $USER@$ip << EOF 
+	cd
+	git clone https://github.com/eubr-atmosphere/fogbow-site-probe.git
+EOF
 
-deploy_file=deploy.yml
-(cd ansible && ansible-playbook $deploy_file)
+deploy_file=setup-conf-files.yml
+(cd ansible && ansible-playbook -v $deploy_file)
 
-img_version=awk -F ' *= *' '$1=="img-version"{print $2}' ./probe-fogbow.conf
-ssh -i ssh_key_file $USER@ip
-(cd ~/fogbow-site-probe && ./run.sh $img_version)
+img_version=`awk -F ' *= *' '$1=="img-version"{print $2}' ./probe-fogbow.conf`
+
+ssh -i $ssh_key_file $USER@$ip << EOF 
+	cd ~/fogbow-site-probe
+	./run.sh $img_version
+EOF
+
 exit 0
--- probes/src/main/java/cloud/fogbow/probes/core/models/Order.java
+++ probes/src/main/java/cloud/fogbow/probes/core/models/Order.java
@@ -44,11 +44,6 @@ public class Order {
     @Size(max = FIELDS_MAX_SIZE)
     private String instanceId;
 
-    @ElementCollection
-    @MapKeyColumn
-    @Column
-    private Map<String, String> requirements = new HashMap<>();
-
     @Column
     @Size(max = FIELDS_MAX_SIZE)
     private String userId;
@@ -60,6 +55,7 @@ public class Order {
     @Column
     @Enumerated(EnumType.STRING)
     protected ResourceType type;
+
 
     public Order() {
     }
@@ -77,6 +73,74 @@ public class Order {
     public Order(String id, String provider, String cloudName, String requester) {
         this(id, provider, cloudName);
         this.requester = requester;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public OrderState getOrderState() {
+        return orderState;
+    }
+
+    public void setOrderState(OrderState orderState) {
+        this.orderState = orderState;
+    }
+
+    public String getRequester() {
+        return requester;
+    }
+
+    public void setRequester(String requester) {
+        this.requester = requester;
+    }
+
+    public String getProvider() {
+        return provider;
+    }
+
+    public void setProvider(String provider) {
+        this.provider = provider;
+    }
+
+    public String getCloudName() {
+        return cloudName;
+    }
+
+    public void setCloudName(String cloudName) {
+        this.cloudName = cloudName;
+    }
+
+    public String getInstanceId() {
+        return instanceId;
+    }
+
+    public void setInstanceId(String instanceId) {
+        this.instanceId = instanceId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getIdentityProviderId() {
+        return identityProviderId;
+    }
+
+    public void setIdentityProviderId(String identityProviderId) {
+        this.identityProviderId = identityProviderId;
+    }
+
+    public ResourceType getType() {
+        return type;
+    }
+
+    public void setType(ResourceType type) {
+        this.type = type;
     }
 
     public String getId() {
