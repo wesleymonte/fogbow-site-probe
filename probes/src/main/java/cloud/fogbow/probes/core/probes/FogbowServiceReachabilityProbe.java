a6e29e691dc7667370dead350cfba0517c0b7b07
Wesley Monte <wesley.monte@ccc.ufcg.edu.br>
2019-08-29 15:23:52 -0300

    Refactored FogbowServiceReachabilityProbe

--- probes/src/main/java/cloud/fogbow/probes/core/probes/FogbowServiceReachabilityProbe.java
+++ probes/src/main/java/cloud/fogbow/probes/core/probes/FogbowServiceReachabilityProbe.java
@@ -2,46 +2,70 @@ package cloud.fogbow.probes.core.probes;
 
 import cloud.fogbow.probes.core.Constants;
 import cloud.fogbow.probes.core.models.Probe;
-import javafx.util.Pair;
-import javax.annotation.PostConstruct;
-import org.springframework.stereotype.Component;
-
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.LinkOption;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import javafx.util.Pair;
+import javax.annotation.PostConstruct;
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Component;
 
 @Component
 public class FogbowServiceReachabilityProbe extends Probe {
 
-    protected int SLEEP_TIME;
-    private int successfulRequests = 0;
+    private static final Logger LOGGER = Logger.getLogger(FogbowServiceReachabilityProbe.class);
     private final int N_REQUESTS_PER_CICLE = 1;
     private final int RESPONSE_CODE_LOWER_BOUND = 199;
     private final int RESPONSE_CODE_UPPER_BOUND = 300;
+    protected int SLEEP_TIME;
+    private int successfulRequests = 0;
     private String AS_ENDPOINT;
     private String RAS_ENDPOINT;
     private String FNS_ENDPOINT;
     private String MS_ENDPOINT;
+    private Map<String, Service> services;
 
     @PostConstruct
     public void FogbowServiceReachabilityProbe() {
         this.lastTimestampAwake = new Timestamp(System.currentTimeMillis());
-        this.probeId = Integer.valueOf(properties.getProperty(Constants.SERVICE_REACHABILITY_PROBE_ID));
+        this.probeId = Integer
+            .valueOf(properties.getProperty(Constants.SERVICE_REACHABILITY_PROBE_ID));
         this.SLEEP_TIME = Integer.valueOf(properties.getProperty(Constants.SLEEP_TIME));
         this.AS_ENDPOINT = properties.getProperty(Constants.AS_ENDPOINT);
         this.RAS_ENDPOINT = properties.getProperty(Constants.RAS_ENDPOINT);
         this.FNS_ENDPOINT = properties.getProperty(Constants.FNS_ENDPOINT);
         this.MS_ENDPOINT = properties.getProperty(Constants.MS_ENDPOINT);
+        this.services = Collections.unmodifiableMap(buildServices());
+    }
+
+    private Map<String, Service> buildServices() {
+        Map<String, Service> services = new HashMap<>();
+        final String AS_ID = "AS";
+        final String RAS_ID = "RAS";
+        final String FNS_ID = "FNS";
+        final String MS_ID = "MS";
+
+        Service AS_SERVICE = new Service(AS_ID, "Authentication Service", AS_ENDPOINT);
+        Service RAS_SERVICE = new Service(RAS_ID, "Resource Allocation Service", RAS_ENDPOINT);
+        Service FNS_SERVICE = new Service(FNS_ID, "Federated Network Service", FNS_ENDPOINT);
+        Service MS_SERVICE = new Service(MS_ID, "Membership Service", MS_ENDPOINT);
+
+        services.put(AS_ID, AS_SERVICE);
+        services.put(RAS_ID, RAS_SERVICE);
+        services.put(FNS_ID, FNS_SERVICE);
+        services.put(MS_ID, MS_SERVICE);
+
+        return services;
     }
 
     @Override
@@ -51,12 +75,12 @@ public class FogbowServiceReachabilityProbe extends Probe {
         while (true) {
             lastTimestampAwake = new Timestamp(System.currentTimeMillis());
 
-            for(int i = 0; i < N_REQUESTS_PER_CICLE; i++) {
+            for (int i = 0; i < N_REQUESTS_PER_CICLE; i++) {
                 doGetRequest();
             }
 
             List<Pair<Number, Timestamp>> data = new ArrayList<>();
-            data.add(new Pair<>(successfulRequests/N_REQUESTS_PER_CICLE, lastTimestampAwake));
+            data.add(new Pair<>(successfulRequests / N_REQUESTS_PER_CICLE, lastTimestampAwake));
 
             List<List<Pair<Number, Timestamp>>> dataWrapper = new ArrayList<>();
             dataWrapper.add(data);
@@ -71,20 +95,23 @@ public class FogbowServiceReachabilityProbe extends Probe {
 
     private void doGetRequest() {
         try {
-            int asResponseCode = getResponseCode(AS_ENDPOINT);
-            int rasResponseCode = getResponseCode(RAS_ENDPOINT);
-            int fnsResponseCode = getResponseCode(FNS_ENDPOINT);
-            int msResponseCode = getResponseCode(MS_ENDPOINT);
-
-            loggerHandler(asResponseCode, rasResponseCode, fnsResponseCode, msResponseCode);
-
-            if (checkRequests(asResponseCode, rasResponseCode, fnsResponseCode, msResponseCode)) {
+            Map<String, Integer> httpCodes = this.getHttpCodes();
+            boolean someFailed = this.checkHttpCodes(httpCodes);
+            if (!someFailed) {
                 successfulRequests++;
             }
-
-        } catch(Exception ex) {
-            ex.printStackTrace();
+        } catch (Exception e) {
+            LOGGER.error("Error while checking reachability of services", e);
         }
+    }
+
+    public Map<String, Integer> getHttpCodes() throws IOException {
+        Map<String, Integer> httpCodes = new HashMap<>();
+        for (Service service : services.values()) {
+            Integer response = getResponseCode(service.ENDPOINT);
+            httpCodes.put(service.ID, response);
+        }
+        return httpCodes;
     }
 
     private int getResponseCode(String endPoint) throws IOException {
@@ -94,59 +121,42 @@ public class FogbowServiceReachabilityProbe extends Probe {
         return connection.getResponseCode();
     }
 
-    private boolean checkRequests(int asResponseCode, int rasResponseCode, int fnsResponseCode, int msResponseCode) {
-        if(asResponseCode < RESPONSE_CODE_UPPER_BOUND && asResponseCode > RESPONSE_CODE_LOWER_BOUND
-            && rasResponseCode < RESPONSE_CODE_UPPER_BOUND && rasResponseCode > RESPONSE_CODE_LOWER_BOUND
-            && fnsResponseCode < RESPONSE_CODE_UPPER_BOUND && fnsResponseCode > RESPONSE_CODE_LOWER_BOUND
-            && msResponseCode < RESPONSE_CODE_UPPER_BOUND && msResponseCode > RESPONSE_CODE_LOWER_BOUND) {
-            return true;
+    public boolean checkHttpCodes(Map<String, Integer> httpCodes) {
+        boolean someFailed = false;
+        for (Entry<String, Integer> code : httpCodes.entrySet()) {
+            Service service = services.get(code.getKey());
+            if (hasFailed(code.getValue())) {
+                String date = timestampToDate(Instant.now().getEpochSecond());
+                LOGGER.error("[" + date + "] : " + service.LABEL + " is down");
+                someFailed = true;
+            }
         }
-
-        return false;
+        return someFailed;
     }
 
     private boolean hasFailed(int responseCode) {
         return responseCode > RESPONSE_CODE_UPPER_BOUND || responseCode < RESPONSE_CODE_LOWER_BOUND;
     }
 
-    private void loggerHandler(int asResponseCode, int rasResponseCode, int fnsResponseCode, int msResponseCode) {
-        List<String> messages = new ArrayList<>();
-
-        if(hasFailed(asResponseCode)) {
-            messages.add("Authentication Service is down");
-        }
-
-        if(hasFailed(rasResponseCode)) {
-            messages.add("Resource Allocation Service is down");
-        }
-
-        if(hasFailed(fnsResponseCode)) {
-            messages.add("Federated Network Service is down");
-        }
-
-        if(hasFailed(msResponseCode)) {
-            messages.add("Membership Service is down");
-        }
-
-        for(int i = 0; i < messages.size(); i++) {
-            String date = timestampToDate(Instant.now().getEpochSecond());
-            String currentMessage = "[" + date + "] " + messages.get(i) + "\n";
-            try {
-                if (!Files.exists(Paths.get("probes-log.log"), LinkOption.NOFOLLOW_LINKS))
-                    Files.createFile(Paths.get("probes-log.log"));
-                Files.write(Paths.get("probes-log.log"), currentMessage.getBytes(), StandardOpenOption.APPEND);
-            } catch (IOException e) {
-                //exception handling left as an exercise for the reader
-            }
-        }
-
-    }
-
-    private String timestampToDate(long timestamp){
-        Date date = new java.util.Date(timestamp*1000L);
+    private String timestampToDate(long timestamp) {
+        Date date = new java.util.Date(timestamp * 1000L);
         SimpleDateFormat sdf = new java.text.SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
         sdf.setTimeZone(java.util.TimeZone.getTimeZone("GMT-3"));
         return sdf.format(date);
     }
+
+    private class Service {
+
+        private final String ID;
+        private final String LABEL;
+        private final String ENDPOINT;
+
+        public Service(String ID, String LABEL, String ENDPOINT) {
+            this.ID = ID;
+            this.LABEL = LABEL;
+            this.ENDPOINT = ENDPOINT;
+        }
+    }
+
 
 }
