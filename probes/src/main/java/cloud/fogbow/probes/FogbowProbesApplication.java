bfb2488af4663591cc171443b505782606b22672
Wesley Monte <wesley.monte@ccc.ufcg.edu.br>
2019-09-05 15:48:43 -0300

    Added conf file path as command line argument

--- probes/src/main/java/cloud/fogbow/probes/FogbowProbesApplication.java
+++ probes/src/main/java/cloud/fogbow/probes/FogbowProbesApplication.java
@@ -3,7 +3,14 @@ package cloud.fogbow.probes;
 import static cloud.fogbow.probes.core.utils.PropertiesUtil.loadProperties;
 
 import cloud.fogbow.probes.core.Constants;
+import java.util.Objects;
 import java.util.Properties;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -16,20 +23,53 @@ public class FogbowProbesApplication {
     private static final Logger LOGGER = LoggerFactory.getLogger(FogbowProbesApplication.class);
 
     public static void main(String[] args) {
+        loadArguments(args);
         SpringApplication.run(FogbowProbesApplication.class, args);
     }
 
     @Bean
     public Properties properties() {
         Properties properties = null;
+        String confFilePath = System.getProperty("conf_file");
+
         try {
-            String path = Thread.currentThread().getContextClassLoader().getResource("").getPath()
-                + "private/";
-            properties = loadProperties(path + Constants.CONF_FILE);
+            if (Objects.isNull(confFilePath)) {
+                confFilePath =
+                    Thread.currentThread().getContextClassLoader().getResource("").getPath()
+                        + "private/";
+                LOGGER.info("Configuration file found in default path " + confFilePath + ".");
+                properties = loadProperties(confFilePath + Constants.CONF_FILE);
+            } else {
+                LOGGER.info("Configuration file found in path " + confFilePath + ".");
+                properties = loadProperties(confFilePath);
+            }
         } catch (Exception e) {
             LOGGER.error("Error while load properties.", e);
             System.exit(1);
         }
         return properties;
+    }
+
+    private static void loadArguments(String[] args){
+        Options options = new Options();
+
+        String opt = "c";
+        String longOpt = "conf_file";
+        String description = "Configuration file path";
+        Option confFilePath = new Option(opt, longOpt, true, description);
+        confFilePath.setRequired(false);
+        options.addOption(confFilePath);
+
+        CommandLineParser parser = new DefaultParser();
+        CommandLine cmd;
+
+        try {
+            cmd = parser.parse(options, args);
+            String inputFilePath = cmd.getOptionValue(longOpt);
+            System.setProperty(longOpt, inputFilePath);
+        } catch (ParseException e) {
+            LOGGER.error("Error while loading command line arguments: " + e.getMessage(), e);
+            System.exit(1);
+        }
     }
 }
