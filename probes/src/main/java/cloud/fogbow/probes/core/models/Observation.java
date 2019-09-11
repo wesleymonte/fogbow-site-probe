6208c85d7045cbb798ea58ca52278a0970a19a6f
Wesley Monte <wesley.monte@ccc.ufcg.edu.br>
2019-09-11 14:44:42 -0300

    Renamed description field to label

--- probes/src/main/java/cloud/fogbow/probes/core/models/Observation.java
+++ probes/src/main/java/cloud/fogbow/probes/core/models/Observation.java
@@ -4,22 +4,22 @@ import org.json.JSONObject;
 
 /**
  * The value entity is basically a structure for storing the result of a measurement. It always has
- * a description for measurement and measurement.
+ * a label for measurement and measurement value.
  */
 public class Observation {
 
-    private static final String DESCRIPTION_JSON_KEY = "description";
+    private static final String LABEL_JSON_KEY = "label";
     private static final String MEASUREMENT_JSON_KEY = "measurement";
-    private String description;
+    private String label;
     private Float measurement;
 
-    public Observation(String description, Float measurement) {
-        this.description = description;
+    public Observation(String label, Float measurement) {
+        this.label = label;
         this.measurement = measurement;
     }
 
-    public String getDescription() {
-        return description;
+    public String getLabel() {
+        return label;
     }
 
     public Float getMeasurement() {
@@ -28,7 +28,7 @@ public class Observation {
 
     public JSONObject toJson() {
         JSONObject json = new JSONObject();
-        json.put(DESCRIPTION_JSON_KEY, this.getDescription());
+        json.put(LABEL_JSON_KEY, this.getLabel());
         json.put(MEASUREMENT_JSON_KEY, this.getMeasurement());
         return json;
     }
