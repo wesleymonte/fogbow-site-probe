6d269d780c20e0408a4e9ca1dfd90e1e7357e206
Wesley Monte <wesley.monte@ccc.ufcg.edu.br>
2019-10-08 14:53:58 -0300

    Added java doc to MetricCollector

--- probes/src/main/java/cloud/fogbow/probes/core/probes/MetricCollector.java
+++ probes/src/main/java/cloud/fogbow/probes/core/probes/MetricCollector.java
@@ -1,6 +1,5 @@
 package cloud.fogbow.probes.core.probes;
 
-import cloud.fogbow.probes.fta.FtaSender;
 import cloud.fogbow.probes.core.models.Metric;
 import cloud.fogbow.probes.core.utils.Pair;
 import java.sql.Timestamp;
@@ -8,13 +7,20 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * It is an entity in charge of making observations at every moment of time ). All observations are
- * sent to the Fogbow Telemetry Aggregator by address {@link #ftaAddress} using {@link FtaSender}.
+ * A metric collector. The user of this interface has access to metric generation according to a
+ * provided timestamp. The collector is used for only one metric type, which must have a name and a
+ * help comment.
  */
 public interface MetricCollector {
 
+    /**
+     * Generates a list of metrics taking into account a timestamp when analyzing data.
+     */
     List<Metric> collect(Timestamp timestamp);
+
     String getMetricName();
+
     String getHelp();
+
     void populateMetadata(Map<String, String> metadata, Pair<String, Float> p);
 }
