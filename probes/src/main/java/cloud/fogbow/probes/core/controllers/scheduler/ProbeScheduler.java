cb0125e8a6c2b3994d0e5a30df435227695ebb80
Wesley Monte <wesley.monte@ccc.ufcg.edu.br>
2019-10-08 14:59:49 -0300

    Added javadoc to submitProbes method

--- probes/src/main/java/cloud/fogbow/probes/core/controllers/scheduler/ProbeScheduler.java
+++ probes/src/main/java/cloud/fogbow/probes/core/controllers/scheduler/ProbeScheduler.java
@@ -4,8 +4,14 @@ import cloud.fogbow.probes.core.probes.Probe;
 import java.util.List;
 
 /**
- * The scheduler is responsible for managing the execution of a set of probes. For the scheduler to run a probe it must be submitted.
+ * The scheduler is responsible for managing the execution of a set of probes. For the scheduler to
+ * run a probe it must be submitted.
  */
 public interface ProbeScheduler {
+
+    /**
+     * The user can send Probes for the scheduler to execute according to their
+     * scheduling policy.
+     */
     void submitProbes(List<Probe> probes);
 }
